bfcfb9d7b34225bcec7ba75bf674ccf7f1d64edd
Daisuke Fujihara <nuklejp@yahoo.co.jp>
2019-07-13 04:36:24 +0000

    16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,19 +14,75 @@ class ProfileController extends Controller
       return view('admin.profile.create');
   }
 
-  public function create()
+  public function create(Request $request)
   {
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      // admin/profile/createにリダイレクトする
       return redirect('admin/profile/create');
   }
+  
+  public function index(Request $request)
+  {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+  }
 
- public function edit()
- {
- return view('admin.profile.edit');
- }
- 
- public function update()
- {
- return redirect('admin/profile/edit');
- }
-    //
+ public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
+  
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
+    Route::get('news', 'Admin\NewsController@index'); // 追記
+    Route::get('news/edit', 'Admin\NewsController@edit'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
+    Route::get('profile', 'Admin\ProfileController@index'); // 追記
+    Route::get('profile/delete', 'Admin\ProfileController@delete');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+
+
+
 });
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
